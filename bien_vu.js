7ade81557595b4fcb677525a628aae47feba8b54
voliveira <vitoroliveiraalmeida97@outlook.com>
2019-01-17 10:58:26 +0100

    v2

--- bien_vu.js
+++ bien_vu.js
@@ -3,6 +3,8 @@ var lastElmt = null;
 var speechInstance = new SpeechSynthesisUtterance("");
 var speaker = window.speechSynthesis;
 
+
+//Récupère le plus ancien parent qui possède au moins un noeud textuel non-vide comme fils
 function getTextParent(elmt) {
 	var parent = elmt.parentNode;
 	var childs;
@@ -19,6 +21,7 @@ function getTextParent(elmt) {
 	return elmt;
 }
 
+//Réinitialise le speaker et le style de la page
 function reset() {
 	speaker.cancel();
 	if (lastElmt) {
@@ -27,41 +30,68 @@ function reset() {
 	}
 }
 
-for (var i = 0, l = pTab.length; i < l; ++i) {
-	pTab[i].addEventListener("mouseover", function(event) {
-		var accept = false;
-		var elmt = null;
-		var msg = "";
+//TextToSpeach du message passe en parametre
+function tts(elmt, msg) {
+	reset();
 
-		if( this.tagName.toLowerCase() === 'a' || this.tagName.toLowerCase() === 'img') {
-			elmt = this;
-			accept = true;
-			msg = this.title !== null && this.title !== "" ? this.title + ". " : "";
-			msg += this.href !== null ? "Lien vers " + this.href : "";
-		}
+	speechInstance = new SpeechSynthesisUtterance(msg);
+	speechInstance.volume = 1;
+	speaker.speak(speechInstance);
 
-		else {
-			elmt = getTextParent(this);
-			if (elmt.textContent !== "" && elmt !== lastElmt) {
-				accept = true;
-				msg = elmt.textContent;
-			}
-		}
+	elmt.style.border = "solid rgb(151, 187, 244) 3px";
+	elmt.style.boxShadow = "0 0 5px rgb(151, 187, 244)";
 
-		if(accept) {
-			reset();
-
-			elmt.style.border = "solid rgb(151, 187, 244) 3px";
-	    	elmt.style.boxShadow = "0 0 5px rgb(151, 187, 244)";
-
-	    	console.log("msg : " + msg);
-
-	    	speechInstance = new SpeechSynthesisUtterance(msg);
-	    	speaker.speak(speechInstance);
-
-	    	lastElmt = elmt;
-
-	    	event.stopPropagation();
-		}
-	}, false);
+	lastElmt = elmt;
 }
+
+
+document.onkeyup = function(e) {
+    if (e.which == 17 && e.which == 18 && e.which == 70)
+        readWithShortcuts("front");
+    else if (e.which == 17 && e.which == 18 && e.which == 74) 
+        readWithShortcuts("back");
+};
+
+function readWithShortcuts(mode){
+    if(speechInstance != null)
+        window.speechSynthesis.cancel(speechInstance);
+
+    if (i != 0 && mode == "back")   
+        i--;
+
+    var accept = false;
+    var elmt = null;
+    var msg = "";
+    //Test si l'element est un lien ou une image
+    if( pTab[i].tagName.toLowerCase() === 'a' || pTab[i].tagName.toLowerCase() === 'img') {
+        elmt = pTab[i];
+        msg = (pTab[i].title && pTab[i].title !== null && pTab[i].title !== "") ? pTab[i].title + ". " : "";
+        console.log(pTab[i].href !== "");
+        msg += (pTab[i].href && pTab[i].href !== null && pTab[i].href !== "") ? "Lien vers " + pTab[i].href : "";
+        tts(elmt, msg);
+    }
+    //Test sur le parent le plus élevé contenant du texte
+    else {
+        elmt = getTextParent(pTab[i]);
+        if (elmt.textContent !== "" && elmt !== lastElmt) {
+            msg = elmt.textContent;
+            tts(elmt, msg);
+        }
+    }
+    event.stopPropagation();
+    
+    if(mode == "front")
+        i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
