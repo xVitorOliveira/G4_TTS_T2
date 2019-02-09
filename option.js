299f5ced1e346470cbee14e08275c3a342b1ef0b
voliveira <vitoroliveiraalmeida97@outlook.com>
2019-02-09 15:02:36 +0100

    v3

--- option.js
+++ option.js
@@ -1,11 +1,6 @@
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
 function saveOptions(e) {
-    /*if(document.querySelector('[name="pwd]"').value)) { 
-       document.getElementById("on").checked; 
-    }else{
-       document.getElementById("off").checked;
-    }*/
     e.preventDefault();
     browser.storage.sync.set({
         onoff: document.querySelector('[name="onoff"]').value,
@@ -13,12 +8,8 @@ function saveOptions(e) {
         voix: document.querySelector('[name="voix"]').value
     });
 }
+
 function restoreOptions() {
-    /*if(document.querySelector('[name="pwd"]').value)) { 
-       document.getElementById("on").checked; 
-    }else{
-       document.getElementById("off").checked;
-    }*/
     function setCurrentChoice(result) {
         document.querySelector('[name="onoff"]').value = result.onoff || true;
     }
@@ -26,14 +17,14 @@ function restoreOptions() {
         document.querySelector('[name="speed"]').value = result.speed || 1;
     }
     function setCurrentChoice3(result) {
-        document.querySelector('[name="voix"]').value = result.voix || "man";
+        document.querySelector('[name="voix"]').value = result.voix || 1;
     }
     function onError(error) {
-        console.log(`Error: ${error}`);
+        console.log(error);
     }
-    var getting = browser.storage.sync.get("onoff");
-    var getting2 = browser.storage.sync.get("speed");
-    var getting3 = browser.storage.sync.get("voix");
+    var getting = this.browser.storage.sync.get("onoff");
+    var getting2 = this.browser.storage.sync.get("speed");
+    var getting3 = this.browser.storage.sync.get("voix");
     getting.then(setCurrentChoice, onError);
     getting2.then(setCurrentChoice2, onError);
     getting3.then(setCurrentChoice3, onError);
--- tests2.js
+++ tests2.js
@@ -1,9 +1,196 @@
-var modeONOFF = true;
-var speedo = 1;
-var voixo = "man";
+var modeONOFF=true;
+var speedo=1;
+var voixo=1;
+
+console.log(modeONOFF);
+console.log(speedo);
+console.log(voixo);
+
+console.log(browser);
+
+
+var getting  = browser.storage.sync.get("onoff");
+var getting2 = browser.storage.sync.get("speed");
+var getting3 = browser.storage.sync.get("voix");
+
+console.log(getting);
+console.log(getting2);
+console.log(getting3);
+
+getting.then(onGot, onError);
+getting2.then(onGot, onError);
+getting3.then(onGot, onError);
+
+console.log(modeONOFF);
+console.log(speedo);
+console.log(voixo);
+
+//Récupération du code source via l'élément body
+var body = document.querySelector("body");
+
+//Initialisation des variables qui réguleront la lecture
+var lastElmt = null;
+var elmtIndex;
+var keyBack = false, keyFront = false, keyStop = false, keyEnter = false, keyRepeat = false, keyAlt = false, keyCtrl = false;
+
+//Initialisation de la voix
+var speechInstance = new SpeechSynthesisUtterance("");
+var speaker = window.speechSynthesis;
+
+//Traitement des images et des liens pour les lires en TTS
+function addSpecials(tab) {
+	var specials = document.querySelectorAll("a, img");
+
+	for (var i = 0; i < specials.length; i++) {
+		if(tab.indexOf(specials[i]) === -1) {
+			tab.push(specials[i]);
+		}
+	}
+
+	return tab;
+}
+
+//Return true si un element du tableau est un noeud texte non vide
+function containsTextNodes(elmts) {
+	for (var i = 0; i < elmts.length; i++) {
+		if(elmts[i].nodeType === Node.TEXT_NODE && elmts[i].textContent.trim() !== "") return true;
+	}
+}
+
+//Initialise le tableau des éléments à lire en TTS
+function getAllTTSChilds(tab, elmt) {
+	var childs = elmt.childNodes;
+
+	if(elmt.textContent !== null && elmt.textContent.trim() !== "") {
+		if((elmt.tagName === "TABLE" || elmt.tagName === "A" || containsTextNodes(childs)) && tab.indexOf(elmt) === -1) {
+			tab.push(elmt);
+		}
+		else {
+			for(var i = 0, l = childs.length; i < l ; i ++) {
+				tab = getAllTTSChilds(tab, childs[i]);
+			}
+		}
+	}
+	return tab;
+}
+
+//Initialisation de elmtTab, qui contient les elements à lire en TTS
+var elmtTab = [];
+elmtTab = getAllTTSChilds(elmtTab, body);
+elmtTab = addSpecials(elmtTab);
+
+//Réinitialise le speaker et le style de la page
+function reset() {
+	speaker.cancel();
+	if (lastElmt) {
+		lastElmt.style.border = 'initial';
+		lastElmt.style.boxShadow = 'initial';
+	}
+}
+
+//Paramétrage de la voix de la voix
+function getSpeechInstance(msg) {
+	var newSI = new SpeechSynthesisUtterance(msg);
+	//newSI.volume = 1;
+    newSI.pitch = voixo;
+	newSI.rate = speedo;
+	return newSI;
+}
+
+//TextToSpeach du message passe en parametre
+function tts(elmt, msg) {
+	reset();
+
+	speechInstance = getSpeechInstance(msg);
+	speaker.speak(speechInstance);
+
+	elmt.style.border = "solid rgb(151, 187, 244) 3px";
+	elmt.style.boxShadow = "0 0 5px rgb(151, 187, 244)";
+
+	lastElmt = elmt;
+}
+
+//Prépare le message à lire, si un message doit être lu
+function prepareForSpeak(elmt) {
+	var msg = null;
+
+	//Test si l'element est un lien ou une image
+	if( elmt.tagName.toLowerCase() === 'a' ) msg = elmt.textContent + ": lien" + (elmt.href ? " vers " + elmt.href : "");
+	if( elmt.tagName.toLowerCase() === 'img' ) msg = "Image" + (elmt.alt ? " : " + elmt.alt : (elmt.title ? " : " + elmt.title : ""));
+	
+	if(msg) tts(elmt, msg);
+
+	else {
+		if (elmt.textContent !== "" && elmt !== lastElmt) {
+			msg = elmt.textContent;
+			tts(elmt, msg);
+		}
+	}
+}
+
+//Active ou désactive les interrupteurs de shortcuts
+function toggleKeys(n, on) {
+	switch (n) {
+		case 13 : keyEnter 	= on; break;
+		case 17 : keyCtrl 	= on; break;
+		case 18 : keyAlt 	= on; break;
+		case 70 : keyBack 	= on; break;
+		case 72 : keyRepeat = on; break;
+		case 74 : keyFront 	= on; break;
+		case 83 : keyStop 	= on; break;
+		default : return;
+	}
+}
+
+//Retourne un index modulé à la taille du tableau
+function getIndex(index, length) {
+	index = index%length;
+	while (index < 0) {
+		index += length;
+	}
+	return index;
+}
+
+//Effectue la fonction demandée par le shortcut
+function readWithShortcuts(mode){
+	elmtIndex = (elmtIndex == null ? 0 : elmtIndex);
+
+	switch (mode) {
+		case "S" 	: speaker.cancel(); break;
+		case "B" 	: elmtIndex --; prepareForSpeak(elmtTab[getIndex(elmtIndex, elmtTab.length)]); break;
+		case "F" 	: elmtIndex ++; prepareForSpeak(elmtTab[getIndex(elmtIndex, elmtTab.length)]); break;
+		case "R" 	: lastElmt = null; prepareForSpeak(elmtTab[getIndex(elmtIndex, elmtTab.length)]); break;
+		case "E"	: if(elmtTab[getIndex(elmtIndex, elmtTab.length)].tagName === "A") { window.open(elmtTab[getIndex(elmtIndex, elmtTab.length)].href,'_blank'); } break;
+		default		: break;
+	}
+}
+
+//Assossie le traitement à faire en fonction du shortcut entré
+function prepareShortCuts() {
+	if (keyStop) {
+		readWithShortcuts("S");
+		keyStop = false;
+	}
+	else if (keyBack) {
+		readWithShortcuts("B");
+		keyBack = false;
+	}
+	else if (keyFront) {
+		readWithShortcuts("F");
+		keyFront = false;
+	}
+	else if (keyRepeat) {
+		readWithShortcuts("R");
+		keyRepeat = false;
+	}
+	else if (keyEnter) {
+		readWithShortcuts("E");
+		keyEnter = false;
+	}
+}
 
 function onError(error) {
-  console.log(`Error: ${error}`);
+     console.log(error);
 }
 
 function onGot(item) {
@@ -15,129 +202,30 @@ function onGot(item) {
         voixo = item.voix;
 }
 
-var getting = browser.storage.sync.get("onoff");
-var getting2 = browser.storage.sync.get("speed");
-var getting3 = browser.storage.sync.get("voix");
-getting.then(onGot, onError);
-getting2.then(onGot, onError);
-getting3.then(onGot, onError);
-
-//speechSynthesisUtteranceInstance.rate = 1.5;
-//speechSynthesisUtteranceInstance.pitch = 1.5;    
-
 if(modeONOFF){
-    var pTab = document.querySelectorAll("body *, a, img");
-    var lastElmt = null;
-    var speechInstance = new SpeechSynthesisUtterance("");
-    var speaker = window.speechSynthesis;
-
-
-    //Récupère le plus ancien parent qui possède au moins un noeud textuel non-vide comme fils
-    function getTextParent(elmt) {
-        var parent = elmt.parentNode;
-        var childs;
-
-        while(parent.tagName && parent.tagName.toLowerCase() !== 'body' && parent!== document) {
-            childs = parent.childNodes;
-
-            for (var i = 0; i < childs.length; i++) {
-                if(childs[i].nodeType === Node.TEXT_NODE && childs[i].textContent !== "") {
-                    return getTextParent(parent);
-                }
-            }
-        }
-        return elmt;
-    }
-
-    //Réinitialise le speaker et le style de la page
-    function reset() {
-        speaker.cancel();
-        if (lastElmt) {
-            lastElmt.style.border = 'initial';
-            lastElmt.style.boxShadow = 'initial';
-        }
-    }
-
-    //TextToSpeach du message passe en parametre
-    function tts(elmt, msg) {
-        reset();
-
-        speechInstance = new SpeechSynthesisUtterance(msg);
-        speechInstance.volume = 1;
-        speaker.speak(speechInstance);
-
-        elmt.style.border = "solid rgb(151, 187, 244) 3px";
-        elmt.style.boxShadow = "0 0 5px rgb(151, 187, 244)";
-
-        lastElmt = elmt;
-    }
-
     //Listener sur les éléments texte
-    for (var i = 0, l = pTab.length; i < l; ++i) {
-        pTab[i].addEventListener("mouseover", function(event) {
-            var accept = false;
-            var elmt = null;
-            var msg = "";
-
-            //Test si l'element est un lien ou une image
-            if( this.tagName.toLowerCase() === 'a' || this.tagName.toLowerCase() === 'img') {
-                elmt = this;
-                msg = (this.title && this.title !== null && this.title !== "") ? this.title + ". " : "";
-                console.log(this.href !== "");
-                msg += (this.href && this.href !== null && this.href !== "") ? "Lien vers " + this.href : "";
-                tts(elmt, msg);
-            }
-
-            //Test sur le parent le plus élevé contenant du texte
-            else {
-                elmt = getTextParent(this);
-                if (elmt.textContent !== "" && elmt !== lastElmt) {
-                    msg = elmt.textContent;
-                    tts(elmt, msg);
-                }
-            }
-
+    for (var i = 0, l = elmtTab.length; i < l; ++i) {
+        elmtTab[i].addEventListener("mouseover", function(event) {
+            elmtIndex = elmtTab.indexOf(this);
+            prepareForSpeak(this);
             event.stopPropagation();
         }, false);
     }
-
-    document.onkeyup = function(e) {
-        if (e.which == 17 && e.which == 18 && e.which == 70)
-            readWithShortcuts("front");
-        else if (e.which == 17 && e.which == 18 && e.which == 74) 
-            readWithShortcuts("back");
-    };
-
-    function readWithShortcuts(mode){
-        if(speechInstance != null)
-            window.speechSynthesis.cancel(speechInstance);
-
-        if (i != 0 && mode == "back")   
-            i--;
-
-        var accept = false;
-        var elmt = null;
-        var msg = "";
-        //Test si l'element est un lien ou une image
-        if( pTab[i].tagName.toLowerCase() === 'a' || pTab[i].tagName.toLowerCase() === 'img') {
-            elmt = pTab[i];
-            msg = (pTab[i].title && pTab[i].title !== null && pTab[i].title !== "") ? pTab[i].title + ". " : "";
-            console.log(pTab[i].href !== "");
-            msg += (pTab[i].href && pTab[i].href !== null && pTab[i].href !== "") ? "Lien vers " + pTab[i].href : "";
-            tts(elmt, msg);
-        }
-        //Test sur le parent le plus élevé contenant du texte
-        else {
-            elmt = getTextParent(pTab[i]);
-            if (elmt.textContent !== "" && elmt !== lastElmt) {
-                msg = elmt.textContent;
-                tts(elmt, msg);
-            }
-        }
-        event.stopPropagation();
-
-        if(mode == "front")
-            i++;
-    }  
 }
 
+//Listener sur les touches enfoncées
+document.addEventListener("keydown", function(e) {
+	if(e.keyCode === 13 || e.keyCode === 17 || e.keyCode === 18 || e.keyCode === 70 || e.keyCode === 72 || e.keyCode === 74 || e.keyCode === 83) {
+		toggleKeys(e.keyCode, true);
+		if(keyAlt && keyCtrl && (keyStop || keyRepeat || keyBack || keyFront || keyEnter)) {
+			prepareShortCuts();
+		}
+	}
+}, false);
+
+//Listener sur les touches qui ne sont plus enfoncées
+document.addEventListener("keyup", function(e) {
+	if(e.keyCode === 13 || e.keyCode === 17 || e.keyCode === 18 || e.keyCode === 70 || e.keyCode === 72 || e.keyCode === 74 || e.keyCode === 83) {
+		toggleKeys(e.keyCode, false);
+	}
+}, false);
